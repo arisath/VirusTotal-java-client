4a9c00e8e3afd31778fa0b661cc903add91e5a6d
Aris <arist.athanas@gmail.com>
2018-09-08 02:23:14 -0700

    Formatted code, optimised dependencies

--- src/main/java/hello/VirusTotal.java
+++ src/main/java/hello/VirusTotal.java
@@ -1,4 +1,22 @@
 package hello;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class VirusTotal {
+
+    public static Resource getUserFileResource() throws IOException {
+        //todo replace tempFile with a real file
+        Path tempFile = Files.createTempFile("upload-test-file", ".txt");
+        Files.write(tempFile, "some test content...\nline1\nline2".getBytes());
+        System.out.println("uploading: " + tempFile);
+        File file = tempFile.toFile();
+        //to upload in-memory bytes use ByteArrayResource instead
+        return new FileSystemResource(file);
+    }
 }
--- src/main/java/hello/VirusTotalController.java
+++ src/main/java/hello/VirusTotalController.java
@@ -1,93 +1,97 @@
 package hello;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-public class VirusTotalController
-{
+public class VirusTotalController {
     @Value("${virusTotalUri}")
     private String uri;
 
     @Value("${apiKey}")
     private String apiKey;
 
-        @RequestMapping("/file/report")
-        public String getFileReport (@RequestParam("fileDigest") String fileHash)
-        {
+
+    @RequestMapping("/file/report")
+    public String getFileReport(@RequestParam("fileDigest") String fileHash) {
 
         RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri+"file/report")
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri + "file/report")
                 .queryParam("apikey", apiKey)
                 .queryParam("resource", fileHash);
 
         String result = restTemplate.getForObject(builder.toUriString(), String.class);
-        return result ;
+        return result;
     }
 
-    @RequestMapping("/file/behaviour")
-    public String getFileBehaviour (@RequestParam("fileDigest") String fileHash)
-    {
+    Ø
+
+    @RequestMapping("/file/scan", method = RequestMethod.POST)
+
+    public String scanFile(@RequestParam("file") String file) {
 
         RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri+"file/behaviour")
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri + "file/scan")
+                .queryParam("apikey", apiKey)
+                .queryParam("file", file);
+
+        String result = restTemplate.getForObject(builder.toUriString(), String.class);
+        return result;
+    }
+
+
+    @RequestMapping("/file/behaviour")
+    public String getFileBehaviour(@RequestParam("fileDigest") String fileHash) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri + "file/behaviour")
                 .queryParam("apikey", apiKey)
                 .queryParam("hash", fileHash);
 
         String result = restTemplate.getForObject(builder.toUriString(), String.class);
-        return result ;
+        return result;
     }
 
 
     @RequestMapping("/url/report")
-    public String getUrlReport (@RequestParam("url") String url)
-    {
+    public String getUrlReport(@RequestParam("url") String url) {
 
         RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri+"url/report")
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri + "url/report")
                 .queryParam("apikey", apiKey)
                 .queryParam("resource", url);
 
         String result = restTemplate.getForObject(builder.toUriString(), String.class);
-        return result ;
+        return result;
     }
 
     @RequestMapping("/ip-address/report")
-    public String getIpAddressReport (@RequestParam("ipAddress") String ipAddress)
-    {
+    public String getIpAddressReport(@RequestParam("ipAddress") String ipAddress) {
 
         RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri+"ip-address/report")
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri + "ip-address/report")
                 .queryParam("apikey", apiKey)
                 .queryParam("ip", ipAddress);
 
         String result = restTemplate.getForObject(builder.toUriString(), String.class);
-        return result ;
+        return result;
     }
 
     @RequestMapping("/domain/report")
-    public String getDomainReport (@RequestParam("domain") String domain)
-    {
+    public String getDomainReport(@RequestParam("domain") String domain) {
 
         RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri+"domain/report")
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri + "domain/report")
                 .queryParam("apikey", apiKey)
                 .queryParam("domain", domain);
 
         String result = restTemplate.getForObject(builder.toUriString(), String.class);
-        return result ;
+        return result;
     }
 
 }
