ea221a3915c53ccea83076e7f72b9ac00fe1732a
Aris <arist.athanas@gmail.com>
2018-09-16 18:37:58 +0100

    Created IP Report POJO

---
+++ src/main/java/hello/FullIpReport.java
@@ -0,0 +1,71 @@
+package hello;
+
+public class FullIpReport {
+
+    private String detected_urls;
+    private String asn;
+    private String country;
+    private String response_code;
+    private String as_owner;
+    private String resolutions;
+    private String verbose_msg;
+
+    public String getDetected_urls() {
+        return detected_urls;
+    }
+
+    public void setDetected_urls(String detected_urls) {
+        this.detected_urls = detected_urls;
+    }
+
+    public String getAsn() {
+        return asn;
+    }
+
+    public void setAsn(String asn) {
+        this.asn = asn;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getResponse_code() {
+        return response_code;
+    }
+
+    public void setResponse_code(String response_code) {
+        this.response_code = response_code;
+    }
+
+    public String getAs_owner() {
+        return as_owner;
+    }
+
+    public void setAs_owner(String as_owner) {
+        this.as_owner = as_owner;
+    }
+
+    public String getResolutions() {
+        return resolutions;
+    }
+
+    public void setResolutions(String resolutions) {
+        this.resolutions = resolutions;
+    }
+
+    public String getVerbose_msg() {
+        return verbose_msg;
+    }
+
+    public void setVerbose_msg(String verbose_msg) {
+        this.verbose_msg = verbose_msg;
+    }
+
+
+
+}
